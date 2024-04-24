95e7a703c2fbb16a232f0dda854ea5d76f29bd71
saadali0987 <slh098098@gmail.com>
2024-04-24 20:24:23 +0500

    Navbar completed!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 
 function App() {
   
 
   return (
-    <div className='  bg-blue-500 h-screen max-w-[640px] md:max-w-[768px] lg:max-w-[1288px] mx-auto pl-[20px] pr-[20px]'>
-      hello world
+    <div className='    border-l-2 border-black border-r-2 h-screen max-w-[640px] md:max-w-[768px] lg:max-w-[1288px] mx-auto pl-[20px] pr-[20px]'>
+      <Navbar />
     </div>
   )
 }
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import logo from "../../assets/logoo.png"
+import menu from "../../assets/menu.png"
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
+  return (
+    <nav className='flex justify-between h-[100px] items-center'>
+        <div className='flex flex-[3]  gap-[50px] items-center'>
+            <a href="/" className='hover:scale-[1.05] ease-in duration-100 flex items-center font-bold  text-[20px] gap-[10px]'>
+                <img className='w-[28px]' src={logo} alt="logo" />
+                <span className='md:hidden lg:inline'>PrimePropertyPortal</span>
+            </a>
+            <a className='hover:scale-[1.05] hidden md:inline ease-in duration-100' href="">Home</a>
+            <a className='hover:scale-[1.05] hidden md:inline ease-in duration-100' href="">About</a>
+            <a className='hover:scale-[1.05] hidden md:inline ease-in duration-100' href="">Contact</a>
+            <a className='hover:scale-[1.05] hidden md:inline ease-in duration-100' href="">Agents</a>
+        </div>
+
+
+        <div className='flex-[2]  flex items-center justify-end h-full lg:bg-gray-200'>
+            <a className=' hidden md:inline hover:scale-[1.05] ease-in duration-100 py-[12px] px-[24px] m-[20px]' href="">Sign Up</a>
+            <a className=' hidden md:inline py-[12px] px-[24px] m-[20px] rounded-md bg-yellow-400  hover:scale-[1.05] ease-in duration-100 ' href="">Sign In</a>
+            <div>
+                <img className={`md:hidden w-[36px] h-[36px] sticky cursor-pointer z-[99]`} onClick={()=>setShowMenu(prev=>!prev)} src={menu} alt="menu" />
+            </div>
+            <div className={`bg-black text-white absolute ease duration-1000 flex flex-col items-center justify-center w-[50%] gap-[50px] size-[28px]  h-screen top-0 ${showMenu ? "right-0" : "right-[-50%]"}`}>
+                <a className='hover:scale-[1.05]' href="">Home</a>
+                <a className='hover:scale-[1.05]' href="">About</a>
+                <a className='hover:scale-[1.05]' href="">Contact</a>
+                <a className='hover:scale-[1.05]' href="">Agents</a>
+                <a className='hover:scale-[1.05]' href="">Sign In</a>
+                <a className='hover:scale-[1.05]' href="">Sign Up</a>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
