597c9ccbae61c3e8d11c29868ad8f53fe55ef220
saadali0987 <slh098098@gmail.com>
2024-04-24 17:04:18 +0500

    responsive layout of the entire app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
 
   return (
-    <div className=' text-blue-500'>
+    <div className='  bg-blue-500 h-screen max-w-[640px] md:max-w-[768px] lg:max-w-[1288px] mx-auto pl-[20px] pr-[20px]'>
       hello world
     </div>
   )
